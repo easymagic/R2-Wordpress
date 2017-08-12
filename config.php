4240a57a4f4f0b0f750bc4cbaf90d0819b815fb9
easymagic <nnamware@yahoo.com>
2017-08-12 03:54:34 -0700

    Update config.php

--- config.php
+++ config.php
@@ -1,9 +1,9 @@
 <?php 
 
-  define('DB_HOST','');
-  define('DB_NAME','');
-  define('DB_USER','');
-  define('DB_PASS','');
+  define('DB_HOST',DB_HOST);
+  define('DB_NAME',DB_NAME);
+  define('DB_USER',DB_USER);
+  define('DB_PASS',DB_PASSWORD);
 
 
   define('DEFAULT_ROUTE', 'home/index');
